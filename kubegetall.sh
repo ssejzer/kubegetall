3bd643416a1c132f618d94dd506c682ad76c2975
ssejzer <ssejzer@techzer.com.ar>
2020-06-02 14:06:43 +0300

    initial commit

---
+++ kubegetall.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+#
+# Get all Kubernetes resources in a namespace or the default namespace if omitted.
+#
+# Usage: kubegetall [namespace]
+#
+
+namespace=${1:-"default"}
+
+for resourceType in $(kubectl api-resources --verbs=list --namespaced -o name); do
+        resources=$(kubectl -n ${namespace} get --ignore-not-found $resourceType)
+        j=0
+        while read -r line; do
+                j=$((j+1))
+                if [ $j -eq 1 ] ; then
+                        header=$line;
+                elif [ $j -eq 2 ] ; then
+                        space=$(echo $resourceType | sed -r 's/./-/g')
+                        echo -e "\n$space $header"
+                        echo -e "$resourceType/$line"
+                else
+                        echo -e "$resourceType/$line"
+                fi
+        done <<< "$resources"
+done
